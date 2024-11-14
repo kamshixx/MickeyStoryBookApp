0be7ad310a3a4314796a4d84c1e5753cf683f5fe
Kamelia Toteva <kamelia.toteva.t@gmail.com>
2024-11-14 12:04:24 +0200

    Initial Commit

---
+++ StoryBookGameApp/StoryBookGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryBookGameAppApp.swift
+//  StoryBookGameApp
+//
+//  Created by Kamelia Toteva on 14.11.24.
+//
+
+import SwiftUI
+
+@main
+struct StoryBookGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoryBookGameAppTests/StoryBookGameAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoryBookGameAppTests.swift
+//  StoryBookGameAppTests
+//
+//  Created by Kamelia Toteva on 14.11.24.
+//
+
+import Testing
+@testable import StoryBookGameApp
+
+struct StoryBookGameAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
